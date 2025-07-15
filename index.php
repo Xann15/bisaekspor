f9575352710a115301d243800c030e7e4fac097d
Xann15 <pandjiealdino15@gmail.com>
2025-07-15 11:38:27 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css" integrity="sha512-oAvZuuYVzkcTc2dH5z1ZJup5OmSQ000qlfRvuoTTiyTBjwX1faoyearj8KdMq0LgsBTHMrRuMek7s+CxF8yE+w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <nav class="navbar fixed-top py-3">
+        <div class="container">
+            <a class="navbar-brand text-white fs-1" href="#">
+                bisa<span style="color: #00A0E4">eksp<span style="color: #F79520;">o</span>r</span>
+            </a>
+        </div>
+    </nav>
+
+    <div class="bg-hero">
+        <img src="assets/img/bg.jpeg" alt="hero">
+    </div>
+    <div class="hero container d-flex">
+        <div class="m-auto">
+            <p class="display-3 text-white">
+                Kami bangga membantu generasi muda untuk <span class="fw-bold">Bisa Ekspor</span>
+            </p>
+            <p class="text-white">Kami bangga telah membantu ribuan anak muda Indonesia untuk menemukan kesuksesan mereka di dunia Ekspor, dan bisa membuka lapangan kerja yang luas untuk masyarakat Indonesia.</p>
+
+            <div class="getApp">
+                <button class="btn p-0">
+                    <img src="assets/img/appstore.png" alt="" style="filter: brightness(100%);">
+                </button>
+                <button class="btn p-0">
+                    <img src="assets/img/playstore.png" alt="" style="filter: brightness(100%);">
+                </button>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="main bg-white d-lg-flex mt-5 mt-lg-0">
+        <div class="quote d-flex col-12 col-lg-6">
+            <div class="title m-auto px-3">
+                <p class="fw-bold display-5">Bisa Ekspor, Bisa Belajar, dan Bisa Cuan</p>
+                <p class="mt-3">Ekspor tidaklah sesulit seperti apa yang dibayangkan oleh kebanyakan orang. Bisa Ekspor merupakan suatu platform digital yang bertujuan untuk meng-Edukasi para generasi muda, betapa mudah dan serunya untuk melakukan Ekspor ke Dunia Luar.</p>
+                <button class="btn btn-primary border shadow border-none fw-bold text-white px-5 py-3">GABUNG BISA EKSPOR</button>
+            </div>
+        </div>
+        <div class="truk mt-5 mt-lg-0 col-12 d-flex col-lg-6">
+            <img src="assets/img/truk.jpg" alt="" class="d-block m-auto">
+        </div>
+    </div>
+
+    <div class="achivement mt-5 mt-lg-0">
+        <p class="title fw-bold fs-2 text-center mb-2">
+            Pencapaian Kami
+        </p>
+        <p class="text-center col-8 mx-auto">
+            Inilah bukti dari kamu untuk anda. Kami berupaya keras untuk menghadirkan layanan ekosistem terbaik untuk masyarakat Indonesia bisa berbisnis ekspor dengan mudah dan cepat.
+        </p>
+
+        <div class="list d-lg-flex gap-2 col-12 col-lg-8 col-md-10 mx-auto mt-5">
+            <div class="card col-10 col-md-6 col-lg-3 mx-auto border border-0 rounded-4 py-lg-4 py-md-0 mb-2 mb-lg-0">
+                <img src="assets/img/hand.png" alt="hand" class="d-block mx-auto img-fluid">
+                <p class="card-text fs-1 fw-bold text-center mb-0">280k+</p>
+                <p class="text-center">Eksportir Bergabung</p>
+            </div>
+            <div class="card col-10 col-md-6 col-lg-3 mx-auto border border-0 rounded-4 py-lg-4 py-md-0 mb-2 mb-lg-0">
+                <img src="assets/img/person.png" alt="person" class="d-block mx-auto img-fluid">
+                <p class="card-text fs-1 fw-bold text-center mb-0">1050+</p>
+                <p class="text-center">Supplier Terdaftar</p>
+            </div>
+            <div class="card col-10 col-md-6 col-lg-3 mx-auto border border-0 rounded-4 py-lg-4 py-md-0 mb-2 mb-lg-0">
+                <img src="assets/img/earth.png" alt="earth" class="d-block mx-auto img-fluid">
+                <p class="card-text fs-1 fw-bold text-center mb-0">35+</p>
+                <p class="text-center">Negara Tujuan Ekspor</p>
+            </div>
+            <div class="card col-10 col-md-6 col-lg-3 mx-auto border border-0 rounded-4 py-lg-4 py-md-0 mb-2 mb-lg-0">
+                <img src="assets/img/box.png" alt="box" class="d-block mx-auto img-fluid">
+                <p class="card-text fs-1 fw-bold text-center mb-0">10+</p>
+                <p class="text-center">Kontainer Berhasil Diekspor</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="app d-lg-flex">
+        <div class="img col-lg-6">
+            <img src="assets/img/app.png" alt="app">
+        </div>
+        <div class="col-lg-6 app-quote d-flex">
+            <div class="m-auto">
+                <p class="fw-bold fs-2 my-4 my-lg-0 col-11 col-lg-8  mx-auto mx-lg-0">
+                    Ayo tunggu apa lagi, segera unduh dan daftarkan dirimu di Aplikasi Bisa Ekspor!
+                </p>
+                <div class="app-getApp mx-4 mx-lg-0 px-3 d-flex gap-2 gap-lg-0">
+                    <button class="btn p-0">
+                        <img src="assets/img/appstore.png" alt="">
+                    </button>
+                    <button class="btn p-0">
+                        <img src="assets/img/playstore.png" alt="">
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="contact mt-5 pt-5">
+        <div class="container d-lg-flex">
+            <div class="left">
+                <div class="logo-brand">
+                    <img src="assets/img/logo.svg" alt="logo">
+                </div>
+                <p>PT Indonesia Bisa Ekspor</p>
+
+                <div class="info">
+                    <div class="email d-flex gap-2">
+                        <i class="bi bi-envelope"></i>
+                        info@bisaekspor.com
+                    </div>
+                    <div class="email d-flex gap-2">
+                        <i class="bi bi-telephone"></i>
+                        (021) 3452841
+                    </div>
+                </div>
+                <div class="link mt-2">
+                    <p>https://bisaekspor.com/</p>
+                </div>
+                <div class="social-media mt-5 mt-lg-0 d-flex gap-4">
+                    <div class="facebook">
+                        <i class="bi bi-facebook fs-2"></i>
+                    </div>
+                    <div class="instagram">
+                        <i class="bi bi-instagram fs-2"></i>
+                    </div>
+                    <div class="twitter-x">
+                        <i class="bi bi-twitter-x fs-2"></i>
+                    </div>
+                    <div class="youtube">
+                        <i class="bi bi-youtube fs-2"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="right mt-5 mt-lg-0">
+                <div class="terms">
+                    <p class="title-terms">INFORMASI</p>
+
+                    <div class="terms-item text-muted">
+                        <p>Syarat dan Ketentuan</p>
+                        <p>Kebijakan Privasi</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer d-flex p-4">
+        <p class="m-auto text-white">
+            &copy; BISA EKSPOR 2023. ALL RIGHTS RESERVED. PT INDONESIA BISA EKSPOR.
+        </p>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
